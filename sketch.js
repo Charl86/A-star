9f1460600c5a16e14adba82c15a2026a291cbfea
Carlos Hernandez <carl.hernan34@gmail.com>
2020-09-07 19:29:46 -0400

    Successfully managed to split javascript files into multiple files. Added a Node class and an A* class, and moved node related methods and members to its respective class as well as A* related methods and members.

--- sketch.js
+++ sketch.js
@@ -1,159 +1,57 @@
 /// <reference path="./p5.global-mode.d.ts" />
 
-
 var cols = 50;
 var rows = 50;
-var grid = new Array(cols);
 
-var openSet = [];
-var closedSet = [];
-var path = [];
-var start;
-var end;
-
-
-class Astar {
-	constructor() {
-
-	}
-}
-
-
-class Node {
-	constructor(x, y) {
-		this.width = width / cols;
-		this.height = height / rows;
-		this.x = x;
-		this.y = y;
-
-		this.fscore = 0;
-		this.gscore = 0;
-		this.hscore = 0;
-
-		this.wall = false;
-		if (random(1) < 0.3)
-			this.wall = true;
-		this.previous = undefined;
-		this.neighbors = [];
-	}
-	show(color, circle=true) {
-		if (circle) {
-			fill(color);
-			if (this.wall)
-				fill(0);
-			noStroke();
-			ellipse(
-				this.x * this.width + this.width / 2,
-				this.y * this.height + this.height / 2,
-				this.width / 2,
-				this.height / 2
-			);
-		}
-		else {
-			fill(color);
-			if (this.wall)
-				fill(0);
-			noStroke();
-			rect(
-				this.x * this.width,
-				this.y * this.height,
-				this.width - 1,
-				this.height - 1
-			);
-		}
-	}
-
-	addNeighbors(grid, diagonal=true) {
-		for (var i = 0; i < 4; i++) {
-			var newX = round(cos(90 * i) + this.x);
-			var newY = round(sin(90 * i) + this.y);
-
-			if ((0 <= newX && newX < cols) && (0 <= newY && newY < rows))
-				this.neighbors.push(grid[newX][newY]);
-
-			var diagonX = round(sqrt(2) * cos(90 * i + 45) + this.x);
-			var diagonY = round(sqrt(2) * sin(90 * i + 45) + this.y);
-			if (diagonal) {
-				if ((0 <= diagonX && diagonX < cols) && (0 <= diagonY && diagonY < rows))
-					this.neighbors.push(grid[diagonX][diagonY]);
-			}
-		}
-	}
-}
-
-
-function removeFromArray(arr, elem) {
-	for (var i = arr.length - 1; i >= 0; i--) {
-		if (arr[i] == elem) {
-			arr.splice(i, 1);
-		}
-	}
-}
-
-
-function heuristic(a, b) {
-	var distance = dist(a.x, a.y, b.x, b.y);
-	// var distance = abs(a.x - b.x) + abs(a.y - b.y);
-	return distance;
-}
+var astar = new Astar();
 
 
 function setup() {
 	createCanvas(540, 540);
 	console.log("A*");
 
-	// Creating 2D array:
-	for (var i = 0; i < cols; i++) {
-		grid[i] = new Array(rows);
-	}
+	// Creating grid that using a 2D-array implementation:
+	astar.createGrid(cols);
 
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			grid[i][j] = new Node(i, j);
-		}
-	}
+	// Making the upmost top-left corner cell the starting point
+	astar.start = astar.grid[0][0];
+	// and the bottom-right cell the goal
+	astar.end = astar.grid[cols - 1][rows - 1];
 
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			grid[i][j].addNeighbors(grid);
-		}
-	}
+	// Unblocking starting and ending cell in case they're blocked:
+	astar.start.blocked = false;
+	astar.end.blocked = false;
 
-	start = grid[0][0];
-	end = grid[cols - 1][rows - 1];
-	start.wall = false;
-	end.wall = false;
-
-	openSet.push(start);
+	astar.openSet.push(astar.start);
 }
 
 function draw() {
-	if (openSet.length > 0) {  // Forward:
-
+	// While there are possible node paths available:
+	if (astar.openSet.length > 0) {
 		var leastCostIdx = 0;
-		for (var i = 0; i < openSet.length; i++) {
-			if (openSet[leastCostIdx].fscore > openSet[i].fscore) {
+		for (var i = 0; i < astar.openSet.length; i++) {
+			if (astar.openSet[leastCostIdx].fscore > astar.openSet[i].fscore) {
 				leastCostIdx = i;
 			}
 		}
-		var currentNode = openSet[leastCostIdx];
+		var currentNode = astar.openSet[leastCostIdx];
 
-		if (currentNode == end) {
+		if (currentNode == astar.end) {
 			noLoop();
 			console.log("Finished.");
 		}
 
-		removeFromArray(openSet, currentNode);
-		closedSet.push(currentNode);
+		astar.rmFromOpenSet(currentNode);
+		astar.closedSet.push(currentNode);
 
 		for (var i = 0; i < currentNode.neighbors.length; i++) {
 			var neighbor = currentNode.neighbors[i];
 
-			if (!closedSet.includes(neighbor) && !neighbor.wall) {
+			if (!astar.closedSet.includes(neighbor) && !neighbor.blocked) {
 				var tempG = currentNode.gscore + 1;
 
 				var newPath = false;
-				if (openSet.includes(neighbor)) {
+				if (astar.openSet.includes(neighbor)) {
 					if (tempG < neighbor.gscore) {
 						newPath = true;
 						neighbor.gscore = tempG;
@@ -162,11 +60,11 @@ function draw() {
 				else {
 					newPath = true;
 					neighbor.gscore = tempG;
-					openSet.push(neighbor);
+					astar.openSet.push(neighbor);
 				}
 
 				if (newPath) {
-					neighbor.hscore = heuristic(neighbor, end);
+					neighbor.hscore = astar.heuristic(neighbor, astar.end);
 					neighbor.fscore = neighbor.gscore + neighbor.hscore;
 					neighbor.previous = currentNode;
 				}
@@ -179,43 +77,43 @@ function draw() {
 		noLoop();
 		return;
 	}
-	var circle = true;  // Draw circle instead of square
 	background(0);
 
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
-			grid[i][j].show(color(255));
+			astar.grid[i][j].show(color(255));
 		}
 	}
-	for (var i = 0; i < closedSet.length; i++) {
-		closedSet[i].show(color(255, 0, 0));
+	for (var i = 0; i < astar.closedSet.length; i++) {
+		astar.closedSet[i].show(color(255, 0, 0));
 	}
-	for (var i = 0; i < openSet.length; i++) {
-		openSet[i].show(color(0, 255, 0));
+	for (var i = 0; i < astar.openSet.length; i++) {
+		astar.openSet[i].show(color(0, 255, 0));
 	}
 
 	// Find the path:
-	path = [];
+	astar.path = [];
 	var temp = currentNode;
-	path.push(temp);
+	astar.path.push(temp);
 	while (temp.previous) {
-		path.push(temp.previous);
+		astar.path.push(temp.previous);
 		temp = temp.previous;
 	}
 
+	var circle = false;  // Draw circle instead of square
 	if (!circle) {
-		for (var i = 0; i < path.length; i++)
-			path[i].show(color(0, 0, 255));
+		for (var i = 0; i < astar.path.length; i++)
+		astar.path[i].show(color(0, 0, 255));
 	}
 	else {
 		noFill();
 		stroke(0, 0, 255);
 		strokeWeight((width/cols)/2);
 		beginShape();
-		for (var i = 0; i < path.length; i++) {
+		for (var i = 0; i < astar.path.length; i++) {
 			vertex(
-				path[i].x * path[i].width + path[i].width / 2,
-				path[i].y * path[i].height + path[i].height / 2
+				astar.path[i].x * astar.path[i].width + astar.path[i].width / 2,
+				astar.path[i].y * astar.path[i].height + astar.path[i].height / 2
 			);
 		}
 		endShape();
